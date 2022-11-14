f4dde06d56fd80b0d337aca3240124a93f42a6af
Azarov Serge <s.a.azarov@gmail.com>
2022-11-14 18:48:02 +0300

    Update lib

--- publish.js
+++ publish.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
-var pkj = require('./package.json');
 
 const publish = async () => {
     console.log('Start script "publish.js"')
     await exec('npm version patch');
-    console.log(`Version up to ${pkj.version}`);
 
     await exec('git add .');
+    const pkj = require('./package.json');
     await exec(`git commit -m "Up v${pkj.version}"`);
 
-    exec('git branch --show-current', async (err, branch, stderr) => {
+    console.log(`Version up to ${pkj.version}`);
+
+    exec('git branch --show-current', async (err, stdout, stderr) => {
         if (err) {
             console.log('Error: ', err);
         }
-    
+        const branch = stdout.replace('\n', '');
         console.log(`Current branch is ${branch}`);
         await exec(`git push origin ${branch}`);
         console.log(`Branch ${branch} pushed`);
