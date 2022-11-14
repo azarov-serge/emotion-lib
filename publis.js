9f8359a2f14fc855a76f2f7409eb9dacecc8c9ed
Azarov Serge <s.a.azarov@gmail.com>
2022-11-14 18:32:19 +0300

    Create script publish

---
+++ publis.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const { exec } = require('child_process');
+
+const publish = async () => {
+    console.log('Start script "publish.js"')
+    await exec('npm version patch');
+
+    exec('git branch --show-current', (err, branch, stderr) => {
+        if (err) {
+            console.log('Error: ', err);
+        }
+    
+        console.log(`Current branch is ${branch}`);
+        exec(`git push origin ${branch}`);
+    
+        console.log('End script "publish.js"')
+    });   
+}
